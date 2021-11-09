b5bcfe79a7e2bc03d5296c9bb9cbd8de407e0fe6
Victor Henrique Monteiro Lima <victorhenriqu3@outlook.com>
2021-11-09 16:21:24 -0400

    Revert "Implementação de Ifrime do Youtube"
    This reverts commit a54f677d5caebd6c937e336b5572f87ecef225f2.

--- script.js
+++ script.js
@@ -1,48 +1,57 @@
 window.onload = function () {
-  update();
+    update()
 };
 
 let PlaylistData = [
-  {
-    name: "O Teatro Mágico - Cinza",
-    link: "https://www.youtube.com/watch?v=zXZCmrNpGzA",
-  },
-  {
-    name: "Scalene - Surreal (CLIPE) | Real / Surreal",
-    link: "https://www.youtube.com/watch?v=0p3EDZso9aY",
-  },
+    {
+        name: "O Teatro Mágico - Cinza",
+        link: "https://www.youtube.com/watch?v=zXZCmrNpGzA"
+    },
+    {
+        name: "Pearl Jam - Black (Official Audio)",
+        link: "https://www.youtube.com/watch?v=qgaRVvAKoqQ"
+    }
 ];
 
-function update() {
-  let tableBody = PlaylistData.map(function ({ name, link }) {
-    let Thumb = link.substring(link.indexOf("=") + 1);
 
-    return `
+
+function update() {
+    let tableBody = PlaylistData.map(
+        function ({ name, link }) {
+            let Thumb = link.substring(link.indexOf("=") + 1);
+
+            return `
             <tr class='PlaylistRow'>
                 <td class="center">${name}</td>
                 <td class="center">
-                <a target="_blank" href='${link}'>
-                <iframe class="LinkVideo" src="https://www.youtube.com/embed/${Thumb}">
-                </a>
-                </iframe>
+                    <a target="_blank" href='${link}'>
+                    <img src='https://img.youtube.com/vi/${Thumb}/1.jpg'/>
+                    </a>
                 </td>
-            </tr>`;
-  });
-  console.log(tableBody);
-  document.querySelector(".PlaylistData").innerHTML = tableBody.join("");
+            </tr>`
+        })
+    console.log(tableBody);
+    document.querySelector(".PlaylistData").innerHTML = tableBody.join('')
 }
 
+
+
+
+
 function SubmitInput() {
-  const nameMusic = document.querySelector(".Name").value;
-  const linkMusic = document.querySelector(".Link").value;
+    const nameMusic = document.querySelector(".Name").value;
+    const linkMusic = document.querySelector(".Link").value;
 
-  if (nameMusic == "" || linkMusic == "") {
-    alert("O Campo não pode ser Nulo ou Vazio. Tente novamente!");
-  } else {
-    PlaylistData = [...PlaylistData, { name: nameMusic, link: linkMusic }];
-    document.querySelector(".Name").value = "";
-    document.querySelector(".Link").value = "";
-  }
+    if (nameMusic == "" || linkMusic == "") {
+        alert("O Campo não pode ser Nulo ou Vazio. Tente novamente!")
+    } else {
+        PlaylistData = [...PlaylistData, { name: nameMusic, link: linkMusic }];
+        document.querySelector(".Name").value = ""
+        document.querySelector(".Link").value = ""
 
-  update();
+    }
+
+
+
+    update()
 }
